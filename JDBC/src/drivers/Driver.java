2c425257173ba2546151ea88532568f2a24f2cbc
farruhhabibullaev <germany-2013@mail.ru>
2017-11-27 18:44:36 -0600

    New Connection to database and query the results

---
+++ JDBC/src/drivers/Driver.java
@@ -0,0 +1,34 @@
+package drivers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Driver {
+    public static void main(String[] args) {
+        try{
+
+            // 1. get connection to database
+            Connection connection = DriverManager.
+                    getConnection("jdbc:mysql://localhost:3306/demo?useSSL=false",
+                            "root","farruh8793");
+            // 2. Create statement
+            Statement  statement = connection.createStatement();
+            // 3. Execute SQL Query
+
+            ResultSet resultSet = statement.executeQuery("select * from employee");
+            // 4. Process the result set
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("first_name") +", "
+                        + resultSet.getString("last_name") +", "
+                        + resultSet.getString("email") + ", "
+                        + resultSet.getString("id"));
+            }
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
