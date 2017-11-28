5eecfe407083f8723cdac8f67a74fc7e362297c0
farruhhabibullaev <germany-2013@mail.ru>
2017-11-28 00:18:14 -0600

    Transactions

--- JDBC/src/drivers/ResultSetMetaData.java
+++ JDBC/src/drivers/ResultSetMetaData.java
@@ -1,19 +1,37 @@
 package drivers;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import config.DbConnection;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ResultSetMetaData {
     public static void main(String[] args) {
         Connection connection = null;
-        java.sql.DatabaseMetaData databaseMetaData = null;
+        Statement statement = null;
+        java.sql.ResultSetMetaData resultSetMetaData = null;
+        ResultSet resultSet = null;
 
         try {
             connection = DbConnection.getConnection();
-            databaseMetaData = connection.getMetaData();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select id,first_name, last_name, salary from employees");
+            resultSetMetaData = resultSet.getMetaData();
+
+            System.out.println("Column count : " +resultSetMetaData.getColumnCount());
+
+            for (int column = 1; column <= resultSetMetaData.getColumnCount() ; column++) {
+                System.out.println("Column name "+ resultSetMetaData.getColumnName(column));
+                System.out.println("Column type name "+ resultSetMetaData.getColumnTypeName(column));
+                System.out.println("Is Nullable "+ resultSetMetaData.isNullable(column));
+                System.out.println(" Is autoincrement" + resultSetMetaData.isAutoIncrement(column));
+
+            }
         } catch (Exception e){
-            
+            System.out.println(e.getMessage());
+
         }
     }
 }
